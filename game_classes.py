a5453692714006263c9b43d7bef5e4ca7bd25116
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-06 00:26:22 +0300

    Update game_classes.py

--- game_classes.py
+++ game_classes.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
-import os
-import telebot
-import time
-import random
-import threading
-from emoji import emojize
-from telebot import types
-from pymongo import MongoClient
+class Game:
+    
+    def __init__(self,m):
+        self.id=m.chat.id
+        self.playerlist={},
+        self.started=False
+        
+    def createplayer(self,user):
+        self.playerlist.update({user.id:{
+            'name':user.first_name,
+            'id':user.id,
+            'role':'good',    #good, evil or unknown (нечто)
+            'effects':[],     #все эффекты типо карантина, заколоченной двери итд
+            'cards':[],       #все карты в руке игрока
+            'alive':True
+        }
+                               })
+        
+    def startgame(self):
+        self.started=True
+        preparegame(self)
+        
+    def preparegame(self):
+        #Тут будет раздача карт игрокам и перемешивание колоды
+        pass
+        
+            
+         
+        
+
 
 
 
