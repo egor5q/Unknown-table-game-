504ce554633f84a3bd16a1409cda9946c4cb3a37
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-08 18:59:07 +0300

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,3 @@
+def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
+    return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
+                                 parse_mode=parse_mode)  
