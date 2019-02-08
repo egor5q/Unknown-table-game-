fe3c01d7e3accd743f311a5bb27f0a10c71fd55b
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-08 23:30:44 +0300

    Update game_classes.py

--- game_classes.py
+++ game_classes.py
@@ -3,6 +3,8 @@ import lobbys
 import tools
 games=lobbys.games
 
+deck=['1','2','3','4','5','6','7','8']
+
 class Game:
     
     def __init__(self,m):
@@ -12,17 +14,13 @@ class Game:
         self.message=None
         self.canceltimer=None
         self.gametimer=None
+        self.deck=deck
+        self.trash=[]
+        self.handcards=4
+        self.currentplayer=None
         
     def createplayer(self,user):
-        self.playerlist.update({user.id:{
-            'name':user.first_name,
-            'id':user.id,
-            'role':'good',    #good, evil or unknown (нечто)
-            'effects':[],     #все эффекты типо карантина, заколоченной двери итд
-            'cards':[],       #все карты в руке игрока
-            'alive':True
-        }
-                               })
+        self.playerlist.update({user.id:Player(user)})
         
     def startgame(self):
         self.started=True
@@ -30,7 +28,25 @@ class Game:
         
     def preparegame(self):
         #Тут будет раздача карт игрокам и перемешивание колоды
-        pass
+        place=1
+        for ids in self.playerlist:
+            self.playerlist[ids]['place']=place
+            i=0
+            while i<self.handcards:
+                x=random.choice(self.deck)
+                self.playerlist[ids]['cards'].append(x)
+                self.deck.remove(x)
+                i+=1
+            place+=1
+        players=[]
+        for ids in self.playerlist:
+            players.append(self.playerlist[ids])
+        self.currentplayer=random.choice(players)
+        self.currentplayer.active=True
+        
+        self.currentplayer.turn()
+        
+        
     
     def m_update(self):   # Обновление списка игроков
         text='Список игроков:\n\n'
@@ -45,7 +61,23 @@ class Game:
         
             
          
+class Player:
+    def __init__(self,user):
+        self.id=id
+        self.name=user.first_name
+        self.id=user.id
+        self.role='good'     #good, evil or unknown (нечто)
+        self.effects=[]      #все эффекты типо карантина, заколоченной двери итд
+        self.cards=[]        #все карты в руке игрока
+        self.alive=True
+        self.place=None
+        self.active=False
         
+    def turn(self):
+        kb=types.InlineKeyboardMarkup()
+        kb.add(types.InlineKeyboardButton(text='Разыграть карту',callback_data='playcard'),types.InlineKeyboardButton(text='Окончить ход',callback_data='endturn'))
+        bot.send_message(self.id, 'Ваша очередь сделать ход.',reply_markup=kb)
+                         
 
 
 
