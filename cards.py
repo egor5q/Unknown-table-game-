8a92fa07caf59a5a506e7ca50ed2b0881c31eaee
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-08 23:05:34 +0300

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,4 +1,10 @@
 import random
 
-
 class Card:
+    def __init__(self,typee):
+        self.type=typee    # infection/action/defence/barrier
+        
+    def use(self):
+        pass
+        
+        
