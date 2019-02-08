8499f58d0ea859d854a94414ae1d5c7953441381
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-08 18:58:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from telebot import types
 from pymongo import MongoClient
 import game_classes
 import lobbys
+games=lobbys.games
 
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
@@ -20,9 +21,32 @@ users=db.users
 
 @bot.message_handler(commands=['creategame'])
 def creategame(m):
-   game=Game(m)
-   games.update({game.id:game})
-
+    if m.chat.id not in games:
+        if m.chat.id!=m.from_user.id:
+            game=Game(m)
+            games.update({game.id:game})
+            kb=types.InlineKeyboardMarkup()
+            kb.add(types.InlineKeyboardButton(text='Присоединиться',callback_data='join'))
+            msg=bot.send_message(m.chat.id,'Набор участников для экспедиции открыт! Жмите "Присоединиться" для вступления в игру.',reply_markup=kb)
+            game=games[m.chat.id]
+            game.message=msg
+         
+         
+@bot.callback_query_handler(func=lambda call:True)
+def inline(call): 
+    if call.data=='join':
+    try:
+        game=games[call.message.chat.id]
+    except:
+        game=None
+    if game!=None:
+        if game.started==False:
+            if call.from_user.id not in game.playerlist:
+                game.createplayer(call.from_user)
+                bot.send_message(call.message.chat.id,call.from_user.first_name+' присоединился!')
+                game.m_update()
+      
+   
 
 print('7777')
 bot.polling(none_stop=True,timeout=600)
