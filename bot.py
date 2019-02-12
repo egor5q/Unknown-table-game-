a39219bdc7059693d28c893440853750169321d4
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-12 15:40:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,72 +68,76 @@ def inline(call):
             bot.send_message(call.message.chat.id, call.from_user.first_name+', в этом чате нет запущенной игры! Сначала начните её '+
                              'командой /creategame.')
     else:
-        kb=types.InlineKeyboardMarkup()
-        game=games[int(call.data.split(' ')[1])]
-        for ids in game.playerlist:
-            if game.playerlist[ids].id==call.from_user.id:
-                user=game.playerlist[ids]
-        chat=game
-        if 'playcard' in call.data:
-            for ids in user.cards:
-                print(ids)
-                print(ids.name)
-                kb.add(types.InlineKeyboardButton(text=ids.name, callback_data='info '+str(chat.id)+' '+ids.code))
-            medit('Выберите карту:', call.message.chat.id, call.message.message_id, reply_markup=kb)
-            
-        if 'info' in call.data:
-            x=call.data.split(' ')[2]
-            text='none'
-            card=None
-            for ids in user.cards:
-                if ids.code==x:
-                    card=ids
-            if card!=None:
-                text=card.info
-                if card.type!='unknown' and card.type!='infection':
-                    kb.add(types.InlineKeyboardButton(text='⚡️Использовать карту', callback_data='usecard '+str(chat.id)+' '+x))
-                kb.add(types.InlineKeyboardButton(text='↩️Назад', callback_data='mainmenu '+str(chat.id)))
-                medit(text, call.message.chat.id, call.message.message_id, reply_markup=kb, parse_mode='markdown')
-            
-        if 'usecard' in call.data:
-            x=call.data.split(' ')[2]
-            card=None
-            for ids in user.cards:
-                if ids.code==x:
-                    card=ids
-            if card!=None:
-                if card.type=='action' or card.type=='barrier':
-                    if user.active:
-                        if card.targetable and user.target==None:
-                            if card.target_all:
-                                enemies=findallenemy(user, game)
-                            else:
-                                enemies=findnearenemy(user, game)
-                            if card.target_self:
-                                enemies.append(user)
-                            for ids in enemies:
-                                kb.add(types.InlineKeyboardButton(text=ids.name, callback_data='usecard '+str(chat.id)+' '+card.code+' '+str(ids.id)))
-                            kb.add(types.InlineKeyboardButton(text='Назад', callback_data='mainmenu'))
-                            medit('Выберите цель для карты "'+card.name+'":', call.message.chat.id, call.message.message_id)
-                        else:
-                            enm=call.data.split(' ')[3]
-                            enemy=None
-                            for ids in chat.playerlist:
-                                if ids.id==int(enm):
-                                    enemy=enm
-                            t=threading.Timer(10, card.use, args=[user, enemy, chat])
-                            t.start()
-                            enemy.defmenu(card)
-                    else:
-                        bot.answer_callback_query(call.id, 'Сейчас не ваш ход!')
-                        
-                elif card.type=='defence':
-                    if user.active==False and user.attacked:
-                        pass
-                    else:
-                        bot.answer_callback_query(call.id, 'Эту карту можно сыграть только в ответ на сыгранную на вас карту!')       
+      try:
+          kb=types.InlineKeyboardMarkup()
+          game=games[int(call.data.split(' ')[1])]
+          for ids in game.playerlist:
+              if game.playerlist[ids].id==call.from_user.id:
+                  user=game.playerlist[ids]
+          chat=game
+          if 'playcard' in call.data:
+              for ids in user.cards:
+                  print(ids)
+                  print(ids.name)
+                  kb.add(types.InlineKeyboardButton(text=ids.name, callback_data='info '+str(chat.id)+' '+ids.code))
+              medit('Выберите карту:', call.message.chat.id, call.message.message_id, reply_markup=kb)
+              
+          if 'info' in call.data:
+              x=call.data.split(' ')[2]
+              text='none'
+              card=None
+              for ids in user.cards:
+                  if ids.code==x:
+                      card=ids
+              if card!=None:
+                  text=card.info
+                  if card.type!='unknown' and card.type!='infection':
+                      kb.add(types.InlineKeyboardButton(text='⚡️Использовать карту', callback_data='usecard '+str(chat.id)+' '+x))
+                  kb.add(types.InlineKeyboardButton(text='↩️Назад', callback_data='mainmenu '+str(chat.id)))
+                  medit(text, call.message.chat.id, call.message.message_id, reply_markup=kb, parse_mode='markdown')
+              
+          if 'usecard' in call.data:
+              x=call.data.split(' ')[2]
+              card=None
+              for ids in user.cards:
+                  if ids.code==x:
+                      card=ids
+              if card!=None:
+                  if card.type=='action' or card.type=='barrier':
+                      if user.active:
+                          if card.targetable and user.target==None:
+                              if card.target_all:
+                                  enemies=findallenemy(user, game)
+                              else:
+                                  enemies=findnearenemy(user, game)
+                              if card.target_self:
+                                  enemies.append(user)
+                              for ids in enemies:
+                                  kb.add(types.InlineKeyboardButton(text=ids.name, callback_data='usecard '+str(chat.id)+' '+card.code+' '+str(ids.id)))
+                              kb.add(types.InlineKeyboardButton(text='Назад', callback_data='mainmenu'))
+                              medit('Выберите цель для карты "'+card.name+'":', call.message.chat.id, call.message.message_id)
+                          else:
+                              enm=call.data.split(' ')[3]
+                              enemy=None
+                              for ids in chat.playerlist:
+                                  if ids.id==int(enm):
+                                      enemy=enm
+                              t=threading.Timer(10, card.use, args=[user, enemy, chat])
+                              t.start()
+                              enemy.defmenu(card)
+                      else:
+                          bot.answer_callback_query(call.id, 'Сейчас не ваш ход!')
+                          
+                  elif card.type=='defence':
+                      if user.active==False and user.attacked:
+                          pass
+                      else:
+                          bot.answer_callback_query(call.id, 'Эту карту можно сыграть только в ответ на сыгранную на вас карту!')       
         
-      
+      except Exception as e:
+          print('Ошибка:\n', traceback.format_exc())
+          bot.send_message(441399484, traceback.format_exc())
+        
    
 
 print('7777')
